fed01b7d739271bb20acfe8afd2dcb61430f031e
Stanislav Smida <s@svagant.com>
2023-12-26 17:44:41 +0100

    Refactor `SourceProvenanced`.

---
+++ Sources/SwiftExtras/SourceProvenance.swift
@@ -0,0 +1,30 @@
+/// A type that includes information about the instantiation source location.
+public protocol SourceProvenanced {
+
+    /// Source location where `Self` was instantiated.
+    var sourceProvenance: SourceProvenance { get }
+}
+
+/// - Note: `#fileID`, `#line` and `#column` macros [allows some type flexibility](https://github.com/apple/swift/blob/f08f86c71617bacbc61f69ce842e284b27036598/stdlib/public/core/Macros.swift#L33).
+/// This type "enforces" type consistency and requires `StaticString`
+/// for `fileID` and `UInt` for `line` and `column` (common in Swift).
+public struct SourceProvenance: CustomStringConvertible {
+
+    public init(_ fileID: StaticString, _ line: UInt, _ column: UInt? = nil) {
+        self.fileID = fileID
+        self.line = line
+        self.column = column
+    }
+
+    public let fileID: StaticString
+    public let line: UInt
+    public let column: UInt?
+
+    public var description: String {
+        if let column {
+            "\(fileID):\(line):\(column)"
+        } else {
+            "\(fileID):\(line)"
+        }
+    }
+}
