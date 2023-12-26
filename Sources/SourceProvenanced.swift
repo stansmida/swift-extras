16b43e925dfb2cbfca3f7cb770ecb42561eb9b14
Stanislav Smida <s@svagant.com>
2023-12-26 14:14:44 +0100

    Added `SourceProvenanced`.

---
+++ Sources/SourceProvenanced.swift
@@ -0,0 +1,16 @@
+/// A type that includes information about the instantiation source location.
+public protocol SourceProvenanced {
+
+    /// Typically "\(#fileID):\(#line)" at where `Self` was instantiated.
+    var sourceProvenance: String { get }
+}
+
+public func sourceProvenance<
+    FileID: ExpressibleByStringLiteral,
+    Line: ExpressibleByIntegerLiteral,
+>(
+    fileID: FileID,
+    line: Line
+) -> String {
+    "\(fileID):\(line)"
+}
